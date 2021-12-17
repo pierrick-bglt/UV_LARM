788e2d8d3c141a43068a5b0be4813c89abee979d
pierrick <pierrick@gmail.com>
2021-12-17 16:32:10 +0100

    ça marche sur le robot

--- challenge1/grp-pourpre/scripts/move_avoid_collision.py
+++ challenge1/grp-pourpre/scripts/move_avoid_collision.py
@@ -11,8 +11,8 @@ marche_avant = True
 tirage = 0
 
 # robot variable 
-STEP_X = 2
-STEP_Z = 1
+STEP_X = 0.2
+STEP_Z = 0.5
 
 # Initialize ROS::node
 rospy.init_node('move', anonymous=True)
@@ -43,7 +43,6 @@ def move_command(data):
         else:
             cmd.angular.z = -STEP_Z  #DROITE
         commandPublisher.publish(cmd)
-        time.sleep(1)
 
 def interpret_scan(data):
     #interpètre les données
@@ -67,7 +66,7 @@ def interpret_scan(data):
 def amIcollision(obstacles):
     #test si le robot est en collisions
     for p in obstacles:     #test toutes les valeurs du dico obstacles
-        if abs(p[0]) <= 0.7 and abs(p[0]) >= 0.05:     #test l'axe des x à 20 cm
+        if abs(p[0]) <= 0.2 and abs(p[0]) >= 0.1:     #test l'axe des x à 20 cm
             #print('x =' + str(p[0]) + 'y =' + str(p[1]))
             #time.sleep(0.5)
             if p[1] <= 0.2 and p[1] >= -0.2: #test l'axe des y pour une ouverture de 90°
